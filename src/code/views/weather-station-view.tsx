69a30e64e08edb1d2e44a091a68594b8ed86afc6
Noah Paessel <knowuh@gmail.com>
2017-11-08 13:56:07 -0500

    Clean up view for demo.

--- src/code/views/weather-station-view.tsx
+++ src/code/views/weather-station-view.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { observer } from "mobx-react";
-import { CardText, CardMedia, CardTitle } from "material-ui/Card";
+import { CardText } from "material-ui/Card";
 import { ComponentStyleMap } from "../utilities/component-style-map";
 import { simulationStore } from "../stores/simulation-store";
 import { IWeatherStation } from "../models/weather-station";
@@ -30,14 +30,14 @@ export class WeatherStationView extends React.Component<
     const time = simulationStore.timeString,
           simulationName = simulationStore.simulationName,
           temperature = weatherStation && weatherStation.temperature,
-          unitTempStr = simulationStore.formatTemperature(temperature, { withUnit: true }),
-          windSpeed = weatherStation && weatherStation.windSpeed,
-          isNonZeroSpeed = windSpeed && isFinite(windSpeed),
-          windSpeedStr = simulationStore.formatWindSpeed(windSpeed, { withUnit: true }),
-          windDirection = weatherStation && weatherStation.windDirection,
-          arrowRotation = (windDirection != null) && isFinite(windDirection)
-                            ? windDirection + 90 : null,
-          arrowChar = isNonZeroSpeed && windDirection ? "\u279B" : "\xA0";
+          unitTempStr = simulationStore.formatTemperature(temperature, { withUnit: true });
+          // windSpeed = weatherStation && weatherStation.windSpeed,
+          // isNonZeroSpeed = windSpeed && isFinite(windSpeed);
+          // windSpeedStr = simulationStore.formatWindSpeed(windSpeed, { withUnit: true }),
+          // windDirection = weatherStation && weatherStation.windDirection,
+          // arrowRotation = (windDirection != null) && isFinite(windDirection)
+          //                   ? windDirection + 90 : null,
+          // arrowChar = isNonZeroSpeed && windDirection ? "\u279B" : "\xA0";
 
     if (weatherStation) {
       name = weatherStation.name;
@@ -49,10 +49,10 @@ export class WeatherStationView extends React.Component<
       card: {
       },
       info: {
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "flex-start"
+        // display: "flex",
+        // flexDirection: "column",
+        // justifyContent: "space-between",
+        // alignItems: "stretch"
       },
       callSign: {
         fontSize: "24pt",
@@ -62,22 +62,12 @@ export class WeatherStationView extends React.Component<
         fontSize: "1pt"
       },
       simulationName: {
-        fontSize: "9pt"
+        fontSize: "9pt",
+        color: "hsla(0, 0%, 80%, 0.9)"
       },
-      media: {
-      },
-      image: {
-        height: 600,
-        maxHeight: 600,
-        objectFit: 'contain'
-      },
-      stats: {
-        display: "flex",
-        fontSize: "3rem",
-        color: "hsla(0, 0%, 100%, 0.9)"
-      },
-      temp: {
-        minWidth: '6em'
+      datum: {
+        marginTop: '1em',
+        marginBottom: '1em'
       },
       time: {
         color: "hsla(0, 0%, 80%, 0.9)"
@@ -85,68 +75,52 @@ export class WeatherStationView extends React.Component<
     };
 
     const settings = simulationStore.settings,
-          showTemperature = settings && settings.showTempValues,
-          showWindValues = settings && settings.showWindValues;
+          showTemperature = settings && settings.showTempValues;
 
     function renderTemperature() {
       return showTemperature
-              ? `Temp: ${unitTempStr}`
+              ? `Temp: ${unitTempStr || '29c'}`
               : null;
     }
 
-    const image = (imageUrl && (imageUrl.length > 1))
-      ? <img style={styles.image} src={imageUrl} />
-      : <div style={styles.image} />;
-
-    function renderWindValues() {
-      return showWindValues
-              ? [
-                  <span key='speed'>
-                    {`Wind: ${windSpeedStr}\xA0`}
-                  </span>,
-                  <span key='direction'
-                        style={{ transform: `rotate(${arrowRotation}deg`, display: 'inline-block'}}>
-                    {arrowChar}
-                  </span>
-              ]
-              : null;
-    }
+    // function renderWindValues() {
+    //   return showWindValues
+    //           ? [
+    //               <span key='speed'>
+    //                 {`Wind: ${windSpeedStr}\xA0`}
+    //               </span>,
+    //               <span key='direction'
+    //                     style={{ transform: `rotate(${arrowRotation}deg`, display: 'inline-block'}}>
+    //                 {arrowChar}
+    //               </span>
+    //           ]
+    //           : null;
+    // }
 
     return (
       <div>
         <CardText>
           <div style={styles.info}>
+            <div style={styles.simulationName}>
+              Simulation Name: {simulationName}
+            </div>
             <div style={styles.callSign}>
-              {callSign}
+              Cell: {callSign}
             </div>
             <div style={styles.name}>
-            {name}
-          </div>
-          <div style={styles.simulationName}>
-            {simulationName}
-          </div>
+              {name}
+            </div>
+            <div style={styles.time}>
+              Time: {time}
+            </div>
+            <div style={styles.datum}>
+              {renderTemperature()}
+            </div>
+            <div style={styles.datum}>
+              Precipitation: clear
+            </div>
         </div>
         </CardText>
-        <CardMedia style={styles.media}
-          overlay={
-            <div>
-              <CardTitle>
-                <div style={styles.stats}>
-                  <div style={styles.temp}>
-                    {renderTemperature()}
-                  </div>
-                  <div style={styles.wind}>
-                    {renderWindValues()}
-                  </div>
-                </div>
-              </CardTitle>
-              <CardText style={styles.time}>
-                {time}
-              </CardText>
-            </div>
-          } >
-          { image }
-        </CardMedia>
       </div>
     );
   }
