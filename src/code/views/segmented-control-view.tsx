1c9178825fb1438e945e149491bc0d7c843f4886
Doug Martin <dmartin@concord.org>
2019-02-01 15:50:14 -0500

    Added 1x, 2x and 3x speed options [#163596423]

--- src/code/views/segmented-control-view.tsx
+++ src/code/views/segmented-control-view.tsx
@@ -3,9 +3,15 @@ import { observer } from "mobx-react";
 import { MuiThemeProvider } from 'material-ui-next/styles';
 import Button from "material-ui-next/Button";
 import Icon from "material-ui-next/Icon";
+import RadioButton from 'material-ui/RadioButton';
+import RadioGroup from 'material-ui/RadioButton/RadioButtonGroup';
 import theme from "../utilities/mui-default-theme";
 import { ComponentStyleMap } from "../utilities/component-style-map";
 import { simulationStore } from "../models/simulation";
+
+// uncomment if enabling renderChangeUpdateIntervalAsSelect()
+// import SelectField from 'material-ui/SelectField';
+// import MenuItem from 'material-ui/MenuItem';
 
 import { TimelineView} from "./timeline-view";
 
@@ -18,6 +24,69 @@ export interface SegmentedControlViewState {
 export class SegmentedControlView extends React.Component<
                                   SegmentedControlViewProps,
                                   SegmentedControlViewState> {
+
+  /*
+
+  keeping in case we want to change from radio buttons to select
+
+  renderChangeUpdateIntervalAsSelect() {
+    const simulationControl = simulationStore.selected && simulationStore.selected.control;
+    if (simulationControl) {
+      const updateIntervalMap:{[key: string]: number} = {
+        '1x': 3,
+        '2x': 6,
+        '3x': 9,
+      };
+      const options = Object.keys(updateIntervalMap).map((key) => <MenuItem key={key} value={updateIntervalMap[key]} primaryText={key} />);
+      const changeUpdateInterval = (e:any, i: number, newInterval:number) => {
+        simulationControl.stop();
+        simulationControl.setUpdateInterval(newInterval);
+      };
+      const selectedInterval = Object.keys(updateIntervalMap).find((key) => updateIntervalMap[key] === simulationControl.updateInterval) || '1x';
+      const value = updateIntervalMap[selectedInterval];
+
+      return (
+        <SelectField
+          value={value}
+          onChange={ (e,i,v) => changeUpdateInterval(e,i,v) }
+          style={{width: 100}}
+          floatingLabelText="Speed"
+        >
+          { options }
+        </SelectField>
+      );
+    }
+  }
+  */
+
+  renderChangeUpdateIntervalAsRadioButtons() {
+    const simulation = simulationStore.selected;
+    if (simulation) {
+      const updateIntervalMap:{[key: string]: number} = {
+        '1x': 0.3333333333,
+        '2x': 0.1666666666,
+        '3x': 0.1111111111,
+      };
+      const lastIndex = Object.keys(updateIntervalMap).length - 1;
+      const options = Object.keys(updateIntervalMap).map((key, index) => {
+        const style = index !== lastIndex ? {marginRight: 20} : {};
+        return <RadioButton key={key} value={updateIntervalMap[key]} label={key} style={style} />;
+      });
+      const changeUpdateInterval = (e:any, newInterval: any) => {
+        simulation.stop();
+        simulation.setUpdateInterval(newInterval);
+      };
+      const updateInterval = simulation.updateInterval;
+      const selectedInterval = Object.keys(updateIntervalMap).find((key) => updateIntervalMap[key] === updateInterval) || '1x';
+      const value = updateIntervalMap[selectedInterval];
+
+      return (
+        <RadioGroup name="speed" onChange={changeUpdateInterval} valueSelected={value} style={{ display: 'flex' }}>
+          { options }
+        </RadioGroup>
+      );
+    }
+  }
 
   render() {
     const simulation = simulationStore.selected;
@@ -42,7 +111,7 @@ export class SegmentedControlView extends React.Component<
         display: "flex",
         flexDirection: "column",
         maxWidth: 500,
-        height: 110,
+        height: 150,
         alignItems: "center"
       },
       buttonStyle: {
@@ -56,6 +125,9 @@ export class SegmentedControlView extends React.Component<
     return(
       <MuiThemeProvider theme={theme}>
         <div style={style.container} >
+          <div style={{marginBottom: 10}}>
+            {this.renderChangeUpdateIntervalAsRadioButtons()}
+          </div>
           <div>
             <Button variant="raised"
               style={style.buttonStyle}
